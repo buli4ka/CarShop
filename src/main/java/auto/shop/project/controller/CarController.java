611101f7c059542ea45f46f53bcd34fe7c6367cb
opalkoandrij@gmail.com <hex_of_andrew_buli4ka>
2021-01-10 00:43:24 +0200

    commit with unworking mysql

--- src/main/java/auto/shop/project/controller/CarController.java
+++ src/main/java/auto/shop/project/controller/CarController.java
@@ -1,11 +1,15 @@
 package auto.shop.project.controller;
 
 
+import auto.shop.project.dto.CarDto;
+import auto.shop.project.exeption.ValidationException;
 import auto.shop.project.service.CarService;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 @Log
@@ -15,6 +19,27 @@ public class CarController {
 
     private final CarService carService;
 
+    @PostMapping("/save")
+    public CarDto saveCars(@RequestBody CarDto carDto) throws ValidationException{
+        log.info("Handling save cars: " + carDto);
+        return carService.saveCar(carDto);
+    }
+    @GetMapping("/findAll")
+    public CopyOnWriteArrayList<CarDto> findAllUsers() {
+        log.info("Handling find all cars request");
+        return carService.findAll();
+    }
 
+    @GetMapping("/findByModel")
+    public CarDto findByLogin(@RequestParam String Model) {
+        log.info("Handling find by model request: " + Model);
+        return carService.findByCarModel(Model);
+    }
 
+    @DeleteMapping("/delete/{Id}")
+    public ResponseEntity<Void> deleteCars(@PathVariable Integer id) {
+        log.info("Handling delete car request: " + id);
+        carService.deleteCar(id);
+        return ResponseEntity.ok().build();
+    }
 }
